f2e02b606069ec2bf109ff5d57a74e5abda2370f
atix <atixan12@gmail.com>
2022-04-04 17:28:00 +0400

    Добавил комментарии для курсов и лекций
    Добавил комментарии для курсов и лекций

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
-
   def create
     comment = commentable.comments.build(comment_params)
     if comment.save
       flash[:notice] = I18n.t('comments.create.success')
-      if commentable.class == 'Course'
+      case commentable.class
+      when 'Course'
         redirect_to promo_course_path
-      elsif commentable.class == 'Lesson'
+      when 'Lesson'
         redirect_to courses_lessons_path
       end
     else
@@ -18,15 +18,15 @@ class CommentsController < ApplicationController
 
   private
 
-    def commentable
-      if params[:lesson_id].nil?
-        @commentable ||= Course.find_by(id: params[:course_id])
-      else
-        @commentable ||= Lesson.find_by(id: params[:lesson_id])
-      end
-    end
+  def commentable
+    @commentable ||= if params[:lesson_id].nil?
+                       Course.find_by(id: params[:course_id])
+                     else
+                       Lesson.find_by(id: params[:lesson_id])
+                     end
+  end
 
-    def comment_params
-      params.require(:comment).permit(:body).merge(user: current_user)
-    end
+  def comment_params
+    params.require(:comment).permit(:body).merge(user: current_user)
+  end
 end
