6b7f1b0a95f7321082463c106cecd58a5f612508
atix <atixan12@gmail.com>
2022-04-08 13:48:03 +0400

    add button

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  validates :course_id, uniqueness: { scope: :user_id }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_082834) do
+ActiveRecord::Schema.define(version: 2022_04_08_065834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_082834) do
     t.bigint "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index %w[course_id user_id], name: "index_orders_on_course_id_and_user_id", unique: true
     t.index ["course_id"], name: "index_orders_on_course_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
