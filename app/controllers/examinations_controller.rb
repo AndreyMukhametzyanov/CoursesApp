eb3f35766561ee837330cca7b7948b3f8512cb84
atix <atixan12@gmail.com>
2022-05-16 17:53:33 +0400

    Доделал прогесс бар и мелкие правки

--- app/controllers/examinations_controller.rb
+++ app/controllers/examinations_controller.rb
@@ -3,7 +3,7 @@
 class ExaminationsController < ApplicationController
   def show
     @examination = Examination.find(params[:id])
-
+    @percent = answers_percentage(@examination)
     if @examination.pass_exam
       redirect_to result_examination_path(@examination)
     else
@@ -15,32 +15,20 @@ class ExaminationsController < ApplicationController
   def check_answer
     examination = Examination.find(params[:id])
     answers = examination.current_question.answers.where(correct_answer: true).pluck(:id)
-    user_answers = params[:user_answers].nil? ? [] : params[:user_answers]['current_question'].map { |answer| answer.to_i }
-
-    puts answers.inspect
-    puts user_answers.inspect
-
+    user_answers = params[:user_answers].nil? ? [] : params[:user_answers]['current_question'].map(&:to_i)
     correct_answer = examination.correct_answers
 
+    correct_answer += 1 if (answers - user_answers).empty?
     if examination.next_question.nil?
-      if (answers - user_answers).empty?
-        correct_answer += 1
-      end
       examination.update(correct_answers: correct_answer,
                          percentage_passing: percent_count(correct_answer, examination.exam.questions.count),
                          pass_exam: true)
     else
-      if (answers - user_answers).empty?
-        correct_answer += 1
-      end
-
       current_question = examination.next_question
       next_question = examination.exam.questions.where('id > ?', current_question.id).first
-
       examination.update(current_question: current_question,
                          next_question: next_question,
                          correct_answers: correct_answer)
-
     end
     redirect_to examination_path(examination.id)
   end
@@ -51,8 +39,12 @@ class ExaminationsController < ApplicationController
 
   private
 
-    def percent_count(correct_answers, number_of_questions)
-      (correct_answers * 100) / number_of_questions
-    end
+  def percent_count(correct_answers, number_of_questions)
+    (correct_answers * 100) / number_of_questions
+  end
+
+  def answers_percentage(model)
+    (model.exam.questions.where('id < ?', model.current_question.id).count.to_f / model.exam.questions.count) * 100
+  end
 
 end
